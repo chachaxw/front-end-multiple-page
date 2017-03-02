2c879d7e6ac3db9806ee12a4536f2917912593d8
Wei Zhou <867571123@qq.com>
2017-03-02 09:14:07 +0800

    配置gulpfile.js，更新package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,27 @@
 const gulp = require('gulp');
-const os = require('os');
-const webpack = require('webpack');
-const uglify = require('gulp-uglify'); // 压缩js
+const $ = require('gulp-load-plugins')({
+  pattern: ['*'],
+  scope: ['devDependencies']
+});
+console.log($);
+
+// package vars
+const pkg = require('./package.json');
+
+// scss - build the scss to the build folder,
+// including the required paths, and writing out a sourcemap
+gulp.task('scss', () => {
+  $.fancyLog("-> Compiling scss:" + pkg.paths.build.css + pkg.vars.scssName);
+  return gulp.src(pkg.paths.src.scss + pkg.vars.scssName)
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+        includePaths: pkg.paths.scss
+      })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer())
+    .pipe($.cached('sass_compile'))
+    .pipe($.sourcemaps.write('./'))
+    .pipe($.size({ gzip: true, showFiles: true }))
+    .pipe(gulp.dest(pkg.paths.build.css));
+})
