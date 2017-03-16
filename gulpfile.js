7dd485dbef7c29b84adb2847e6f09798b227dc56
Wei Zhou <867571123@qq.com>
2017-03-16 12:46:37 +0800

    添加 autoprefixer 配置，修改 gulp 配置

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const $ = require('gulp-load-plugins')({
   scope: ['devDependencies']
 });
 const browserSync = $.browserSync.create();
+const watch = $.watch;
 
 // package vars
 const pkg = require('./package.json');
@@ -11,19 +12,19 @@ const pkg = require('./package.json');
 // scss - build the scss to the build folder,
 // including the required paths, and writing out a sourcemap
 gulp.task('sass', () => {
-  $.fancyLog("-> Compiling scss: " + pkg.paths.build.css + pkg.vars.scssName);
-  return gulp.src(pkg.paths.src.scss + pkg.vars.scssName)
-    .pipe($.plumber({ errorHandler: onError }))
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-            includePaths: pkg.paths.scss
-        })
-        .on('error', $.sass.logError))
-    .pipe($.cached('sass_compile'))
-    .pipe($.autoprefixer())
-    .pipe($.sourcemaps.write('./'))
-    .pipe($.size({ gzip: true, showFiles: true }))
-    .pipe(gulp.dest(pkg.paths.build.css));
+  $.fancyLog("-> Compiling scss: " + pkg.paths.src.sass + pkg.vars.scssName);
+  return watch(pkg.globs.sass, () => {
+    gulp.src(pkg.paths.src.sass + pkg.vars.scssName)
+      .pipe($.plumber({ errorHandler: onError }))
+      .pipe($.sourcemaps.init())
+      .pipe($.sass({ includePaths: pkg.paths.scss})
+      .on('error', $.sass.logError))
+      .pipe($.cached('sass_compile'))
+      .pipe($.autoprefixer())
+      .pipe($.sourcemaps.write('./'))
+      .pipe($.size({ gzip: true, showFiles: true }))
+      .pipe(gulp.dest(pkg.paths.src.css));
+  });
 });
 
 // Error handler
